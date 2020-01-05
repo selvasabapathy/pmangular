47a3f079203db5f53cd09c9473e64c93c4b3c141
Selva Sabapathy <selva.sabapathy@ihg.com>
2020-01-05 11:36:32 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms'
+
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MenuComponent } from './menu/menu.component';
+import { UserComponent } from './user/user.component';
+import { ProjComponent } from './proj/proj.component';
+import { TaskAddComponent } from './task-add/task-add.component';
+import { TaskUpdateComponent } from './task-update/task-update.component';
+import { TaskViewComponent } from './task-view/task-view.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    MenuComponent,
+    UserComponent,
+    ProjComponent,
+    TaskAddComponent,
+    TaskUpdateComponent,
+    TaskViewComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
