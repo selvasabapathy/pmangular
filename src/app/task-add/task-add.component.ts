534c55e2b18837cc1126261703147bf6c9c609b5
Selva Sabapathy <selva.sabapathy@ihg.com>
2020-01-15 04:05:29 -0500

    Complete Task Add/Edit UI

--- src/app/task-add/task-add.component.ts
+++ src/app/task-add/task-add.component.ts
@@ -5,13 +5,14 @@ import { User } from '../user/user.component';
 import { UserService } from '../service/user.service';
 import { Project } from '../proj/proj.component';
 import { ParentTaskService } from '../service/parent-task.service';
+import { TaskService } from '../service/task.service';
 
 export class ParentTask {
   constructor(public parentTaskId: number, public name: string) {}
 }
 
 export class Task {
-  constructor(public taskId: number, public parentTaskId: number, public projectId: number, public name: string, public startDate: Date, public endDate: Date, public priority: number, public status: boolean) {}
+  constructor(public taskId: number, public userId: number, public parentTaskId: number, public projectId: number, public name: string, public startDate: Date, public endDate: Date, public priority: number, public status: boolean) {}
 }
 
 @Component({
@@ -22,22 +23,25 @@ export class Task {
 export class TaskAddComponent implements OnInit {
   addOrUpdateButtonText: string = 'Add';
 
+  projectId: number;
   project: Project;
   projects: Project[];
 
   task: Task;
   isParentTask: boolean;
 
+  parentTaskId: number;
   parentTask: ParentTask;
   parentTasks: ParentTask[];
 
   userId: number;
+  user: User;
   users: User[];
 
   isStartDateError: boolean;
   isEndDateError: boolean;
 
-  constructor(private projService: ProjService, private parentTaskService: ParentTaskService, private userService: UserService) { }
+  constructor(private projService: ProjService, private parentTaskService: ParentTaskService, private taskService: TaskService, private userService: UserService) { }
 
   ngOnInit() {
     console.log(`TaskAddComponent - ngOnInit`);
@@ -50,66 +54,103 @@ export class TaskAddComponent implements OnInit {
   validtateDates() {
     this.isStartDateError = false;
     this.isEndDateError = false;
-    if (new Date(this.project.startDate) < new Date()) {
+    if (new Date(this.task.startDate) < new Date()) {
       this.isStartDateError = true;
     }
-    if (new Date(this.project.endDate) <= new Date() || new Date(this.project.endDate) < new Date(this.project.startDate)) {
+    if (new Date(this.task.endDate) <= new Date() || new Date(this.task.endDate) < new Date(this.task.startDate)) {
       this.isEndDateError = true;
     }
   }
 
-  pickProjectId() {
-    console.log(`Manager UserId: ${this.userId}`);
+  pickProjectId() : void {
+    console.log(`pickProjectId: ${this.projectId}`);
   }
 
-  isParentTaskFlipped() {
+  pickParentTaskId() : void {
+    console.log(`pickParentTaskId: ${this.parentTaskId}`);
+  }
+
+  pickUserId() : void {
+    console.log(`pickUserId: ${this.userId}`);
+  }
+
+  isParentTaskFlipped() : void {
+    console.log(`isParentTaskFlipped: before ${this.isParentTaskFlipped}`);
     this.isParentTask != this.isParentTask;
   }
 
   resetForm() : void {
     console.log(`Reset form...`);
+    this.refreshFields();
     this.refreshProjects();
+    this.refreshParentTasks();
+    this.refreshUsers();
   }
 
-  addTask() : void {
-    console.log(`Project Manager add/update (Manager (UserID): ${this.userId}):  ${this.project}`);
-    if (this.project.projectId === 0) {
-      this.projService.addProject(this.userId, this.project).subscribe(
+  addParentTask() : void {
+    this.parentTask = new ParentTask(this.task.taskId, this.task.name);
+    if (this.parentTask.parentTaskId === 0) {
+      console.log(`ParentTask Add: ${this.parentTask}`);
+      this.parentTaskService.addParentTask(this.parentTask).subscribe(
         response => {
+          this.refreshFields();
           this.refreshProjects();
           this.refreshParentTasks();
           this.refreshUsers();
         }
       )  
     } else {
-      console.log(`Edit project: ` + this.project);
+      console.log(`ParentTask Edit: ${this.task}`);
       this.addOrUpdateButtonText = 'Add';
-      this.projService.editProject(this.userId, this.project).subscribe(
+      this.parentTaskService.editParentTask(this.parentTask.parentTaskId, this.parentTask).subscribe(
         response => {
+          this.refreshFields();
           this.refreshProjects();
           this.refreshParentTasks();
           this.refreshUsers();
         }
       )  
     }
+    console.log(`ParentTask Add/Edit - Sent request to service`);
   }
 
-  editTask(projectId: number) : void {
-    console.log(`Edit project: ${projectId}`);
-    this.addOrUpdateButtonText = 'Update';
-    this.project = this.projects.find(project => project.projectId === projectId);
-    this.userService.retrieveManager(projectId).subscribe(
-      response => {
-        console.log(`Manager of the projectId ${projectId} is ${response.toString}`);
-        if (response.length === 0) {
-          console.log(`Manager of the projectId ${projectId} is NONE`);
-          this.userId = 0;
-        } else {
-          // this.managers = response;
-          // this.userId = this.managers[0].userId;
-        }
+  addTask() : void {
+    if (this.isParentTask) {
+      console.log(`Parent Task`);
+      this.addParentTask();
+    } else {
+      this.task.userId = this.userId;
+      this.task.projectId = this.projectId;
+      this.task.parentTaskId = this.parentTaskId;
+      if (this.task.taskId === 0) {
+        console.log(`Task Add: ${this.task}`);
+        this.taskService.addTask(this.task).subscribe(
+          response => {
+            this.refreshFields();
+            this.refreshProjects();
+            this.refreshParentTasks();
+            this.refreshUsers();
+          }
+        )  
+      } else {
+        console.log(`Task Edit: ${this.task}`);
+        this.addOrUpdateButtonText = 'Add';
+        this.taskService.editTask(this.task.taskId, this.task).subscribe(
+          response => {
+            this.refreshFields();
+            this.refreshProjects();
+            this.refreshParentTasks();
+            this.refreshUsers();
+          }
+        )  
       }
-    )
+      console.log(`Task Add/Edit - Sent request to service`);
+    }
+  }
+
+  editTask(taskId: number) : void {
+    console.log(`Edit Task: ${taskId}`);
+    console.log(`Edit Task: Not used... use add() instead`);
   }
 
   // deleteProject(projectId: number) : void {
@@ -123,12 +164,13 @@ export class TaskAddComponent implements OnInit {
   // }
 
   refreshFields() : void {
-    this.task = new Task(0,0,0,'',new Date(), new Date(), 0, false);
+    this.task = new Task(0,0,0,0,'',new Date(), new Date(), 0, false);
     this.isParentTask = false;
     this.addOrUpdateButtonText = 'Add';
   }
 
   refreshProjects() : void {
+    this.projectId = 0;
     this.project =  new Project(0,null,0,null, null, 0, false);
     this.projService.retrieveAllProjects().subscribe(
       response => {
@@ -139,6 +181,7 @@ export class TaskAddComponent implements OnInit {
   }
 
   refreshParentTasks() : void {
+    this.parentTaskId = 0;
     this.parentTask =  new ParentTask(0,null);
     this.parentTaskService.retrieveAllParentTasks().subscribe(
       response => {
@@ -150,7 +193,7 @@ export class TaskAddComponent implements OnInit {
 
   refreshUsers() : void {
     this.userId = 0;
-    // this.managers = null;
+    this.user = new User(0,0,'','');
     this.userService.retrieveAllUsers().subscribe(
       response => {
         console.log(`Users from service: ${response}`);
